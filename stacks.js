4b2934644bb4df691bd448c32f60fc4d97aaf806
jingwen-wu0601 <61522389+jingwen-wu0601@users.noreply.github.com>
2021-04-05 19:47:14 -0700

    update

--- stacks.js
+++ stacks.js
@@ -1,5 +1,18 @@
 // functions: push, pop, peak, length
 
 class Stack {
-  constructor()
+  constructor(count = 0, storage = []) {
+    this.count = count;
+    this.storage = storage;
+  }
+
+  Add(value) {
+    this.count++;
+    this.storage.push(value)
+  }
 }
+
+var mystack = new Stack();
+mystack.Add(12)
+mystack.Add("letter")
+console.log(mystack.storage)
